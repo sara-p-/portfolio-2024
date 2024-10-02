ad8b0206c9b04506a22279cca950f0dcf7c76172
sara-p- <sarapitt88@gmail.com>
2024-10-02 13:57:25 -0400

    working on the title animation.

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,7 +1,7 @@
 // import { useRef } from 'react'
 import './title.css'
-import { splitTitle } from './title-utils'
-import { useEffect, useRef } from 'react'
+import { getLettersWidthAndHeight, splitTitle } from './title-utils'
+import { useEffect, useRef, useState } from 'react'
 
 type titleProps = {
   firstLine: string
@@ -9,41 +9,58 @@ type titleProps = {
 }
 
 function Title({ firstLine, secondLine }: titleProps) {
+  // Get/Set the value of the word/letter measurements (through the useEffect hook)
+  const [firstWord, setFirstWord] = useState({
+    wordHeight: 0,
+    wordWidth: 0,
+    letterWidths: [0],
+  })
+  const [secondWord, setSecondWord] = useState({
+    wordHeight: 0,
+    wordWidth: 0,
+    letterWidths: [0],
+  })
+  // Grab a reference to the span elements that hold the letters
+  const firstLetters = useRef<HTMLSpanElement[]>([])
+  const secondLetters = useRef<HTMLSpanElement[]>([])
   // Split the words into an array of letters
   const first: { id: string; letter: string }[] | undefined =
     splitTitle(firstLine)
   const second: { id: string; letter: string }[] | undefined =
     splitTitle(secondLine)
-  // Grab a reference to the span elements that hold the letters
-  const firstRefs = useRef<HTMLSpanElement[]>()
 
-  // console.log({ firstRefs })
+  // Calculate the pixel width of each letter span element and add it to an array
+  useEffect(() => {
+    if (!firstLetters.current) {
+      return
+    }
 
-  // function getRefs(theRef: HTMLSpanElement, index) {
-  //   if(firstRefs.current) {
-  //     return (firstRefs.current[index] = theRef)
-  //   }
-  //   return
-  // }
+    // Get the height and width of the word, as well as the individual letters
+    // const firstWordMeasurements = getLettersWidthAndHeight(firstLetters)
+    // const firstObject = getLettersWidthAndHeight(firstLetters)
+    setFirstWord(getLettersWidthAndHeight(firstLetters))
+    if (secondWord) {
+      // const secondWordMeasurements = getLettersWidthAndHeight(secondLetters)
+      setSecondWord(getLettersWidthAndHeight(secondLetters))
+    }
+  }, [secondWord])
 
   return (
     <div className='title-box'>
       <h1 className='title'>
-        <span className='word first-line'>
+        <span
+          className='word first-line'
+          style={{ width: `${firstWord.wordHeight}px` }}
+        >
           {first &&
             first.map((item, index) => {
               return (
                 <span
                   key={item.id}
                   className='letter'
+                  style={{}}
                   ref={(ref) => {
-                    if (ref) {
-                      if (firstRefs.current) {
-                        console.log(firstRefs.current)
-
-                        return (firstRefs.current[index] = ref)
-                      }
-                    }
+                    if (ref) firstLetters.current[index] = ref
                   }}
                 >
                   {item.letter}
@@ -53,9 +70,15 @@ function Title({ firstLine, secondLine }: titleProps) {
         </span>
         {second && (
           <span className='word second-line'>
-            {second.map((item) => {
+            {second.map((item, index) => {
               return (
-                <span key={item.id} className='letter'>
+                <span
+                  key={item.id}
+                  className='letter'
+                  ref={(ref) => {
+                    if (ref) secondLetters.current[index] = ref
+                  }}
+                >
                   {item.letter}
                 </span>
               )
--- src/components/Title/title-utils.ts
+++ src/components/Title/title-utils.ts
@@ -1,5 +1,5 @@
 // Turn the title string into an array of letters
-export const splitTitle = (title: string | undefined) => {
+export function splitTitle(title: string | undefined) {
   if (!title) {
     return undefined
   }
@@ -14,4 +14,42 @@ export const splitTitle = (title: string | undefined) => {
     })
   }
   return letterArrayObject
+}
+
+// Grab the refs of the letters and return the pixel widths
+export function getLetterWidths(
+  refs: React.MutableRefObject<HTMLSpanElement[]>
+) {
+  let widths: number[]
+  if (refs.current) {
+    widths = refs.current?.map((ref) => {
+      if (ref) {
+        return ref.getBoundingClientRect().width
+      }
+      return 0
+    })
+  } else {
+    widths = [0]
+  }
+
+  return widths
+}
+
+// Accept the ref array of letters, and return the height and widths
+export function getLettersWidthAndHeight(
+  refs: React.MutableRefObject<HTMLSpanElement[]>
+) {
+  // Get the height of the letters to use as the height of the words
+  // const wordHeight = refs.current[0].getBoundingClientRect().height
+  const wordHeight = 0
+  // Get the widths of each letter in the word
+  const letterWidths = getLetterWidths(refs)
+  console.log(letterWidths)
+
+  // Get the sum of all the letter widths to figure out the total width of the word
+  const wordWidth = letterWidths.reduce(
+    (accumulator: number, current: number) => accumulator + current
+  )
+
+  return { wordHeight, letterWidths, wordWidth }
 }
