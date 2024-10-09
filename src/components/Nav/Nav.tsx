70596775ee60700b7056870858c616dc8b594b40
sara-p- <sarapitt88@gmail.com>
2024-10-09 13:48:58 -0400

    about to tear some stuff up to try out forwardRef. Trying to make the header scrollTo functionality fancy.

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,27 +1,34 @@
+import './nav.css'
 import Link from '../Link/Link'
 import Toggle from '../Toggle/Toggle'
-import './nav.css'
+import { MENU_ARRAY } from '../../globals/global-variables'
 
 interface navProps {
   label: string
 }
 
 function Nav({ label }: navProps) {
+  function handleNavClick(e): void {
+    e.preventDefault()
+    // const element: HTMLDivElement | null = document.getElementById(sectionName)
+    // const headerHeight = document.querySelector('header').offsetHeight // Get the height of your header
+
+    // window.scrollTo({
+    //   top: element.offsetTop - headerHeight,
+    //   behavior: 'smooth', // Optional for smooth scrolling
+    // })
+  }
+
   return (
     <nav className='nav' aria-label={label}>
       <ul className='menu'>
-        <li className='menu-item'>
-          <Link href={'#home'} linkText={'home'} />
-        </li>
-        <li className='menu-item'>
-          <Link href={'#work'} linkText={'work'} />
-        </li>
-        <li className='menu-item'>
-          <Link href={'#about'} linkText={'about'} />
-        </li>
-        <li className='menu-item'>
-          <Link href={'#contact'} linkText={'contact'} />
-        </li>
+        {MENU_ARRAY.map((item) => {
+          return (
+            <li className='menu-item'>
+              <Link href={`#${item}`} linkText={item} />
+            </li>
+          )
+        })}
       </ul>
       <Toggle />
     </nav>
--- src/globals/global-variables.ts
+++ src/globals/global-variables.ts
@@ -1,1 +1,3 @@
 export const ENDPOINT: string = '/data/data.json'
+// Menu links
+export const MENU_ARRAY: string[] = ['home', 'work', 'about', 'contact']
